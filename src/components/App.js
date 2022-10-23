d11c3e473867a49df3d72148e8fcfacabc2f6f4c
+ <+>
2022-10-23 16:49:09 -0400

    adds browser router and tentative routes setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
 // import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import NavBar from './NavBar';
 import Login from './Login';
 import JournalForm from './JournalForm';
@@ -21,12 +28,26 @@ function App() {
   return (
     <div className="App">
       hello
-    <NavBar />
-    <Login />
-    <JournalForm />
-    <JournalList />
-    <JournalPreview />
-  
+
+    <Router>
+      <NavBar />
+      <LogIn />
+      <Switch>
+      
+        {/* <Login /> */}
+            
+        <Route exact path="/journal_entries/new" render={() => <JournalForm setJournallist={setJournallist} /> } />
+       
+        {/* <Route exact path="/">
+          <LogIn /> or <Home />
+        </Route>      */}
+      
+        <Route exact path="/journal_entries" render={() => <JournalList journallist={journallist} />} />
+           
+        <Route exact path="/journal_entries/:id" render={() => <JournalPreview /> } />
+        
+      </Switch>
+    </Router>
     </div>
   );
 }
