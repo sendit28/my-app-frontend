824331eb18ddae1d7985182eed9dcd2efb96cae6
+ <+>
2022-10-23 13:09:05 -0400

    creates start of journal previe component

---
+++ src/components/JournalPreview.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { useParams} from 'react-router-dom'
+
+function JournalPreview() {
+  const [journal, setJournal] = useState({})
+  const { id } = useParams()
+  const { title, date, content } = journal 
+    // including category?
+
+  useEffect (() => {
+    fetch(`http://localhost:9292/journal_entries/${id}`)
+      .then(r => r.json())
+      .then(data => setJournal(data))
+  }, [])
+  
+  
+  
+  return (
+    <div className="journal-preview">
+
+
+      <label htmlFor= "title"> Title </label>
+      <p>{title}</p> 
+    
+      <label htmlFor= "date"> Date </label>
+      <p>{date}</p> 
+
+      <label htmlFor= "content"> Content </label>
+      <p>{content}</p> 
+
+      {/* <button onClick=handleUpdateClick}> Update </button>
+      <button on Click=handleDeleteClick}> Delete </button>
+     */}
+    </div>
+  )
+}
+
+export default JournalPreview
