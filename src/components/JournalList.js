50bf53d5012b5d68a6661bd2b3c2724bc2402cf8
+ <+>
2022-10-23 11:13:35 -0400

    adds to journal card return error

--- src/components/JournalList.js
+++ src/components/JournalList.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import JournalCard from './JournalCard'
 
-function JournalList({ journallist}) {
+function JournalList({ journallist }) {
 
   const journalentries = journallist.map((entry) =>
     <JournalCard id={entry.id} title={entry.title} content={entry.content} date={entry.date} />
