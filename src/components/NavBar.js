054a1709981367ce9bf53932498d6d9db52c9e0b
+ <+>
2022-10-23 16:46:44 -0400

    adds links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { link } from "react-router-dom";
 
 function NavBar() {
+ 
+ 
+ 
+ 
+ 
+ 
   return (
-    <div className="nav-bar">NavBar</div>
+    <div className="nav-bar">NavBar
+      <div>
+        <Link to="/"> Home </Link>
+      </div>
+      <div>
+        <Link to="/journal_entries"> Journal </Link>
+      </div>
+      <div>
+        <Link to="journal_entries/new"> New Entry </Link>
+      </div>
+    </div>
   )
 }
 
