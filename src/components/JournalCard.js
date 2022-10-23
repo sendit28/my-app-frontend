2e9389702a3d8be51ec5cef88dceb02e2ec10f5b
+ <+>
2022-10-23 11:14:13 -0400

    creates journal card

---
+++ src/components/JournalCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+function JournalCard({ id, title, content, date }) {
+  
+  let history = useHistory()
+  
+  function handleClick() {
+    history.push(`/journal_entries/${id}`)
+  }
+  
+  
+  return (
+    <div className="journal-card">
+      <h3>{title} onClick={handleClick}</h3>
+      <h4>{date}</h4>
+    </div>
+  )
+}
+
+// all in one element? h4...
+export default JournalCard
