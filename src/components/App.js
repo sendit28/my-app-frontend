5bdd140fe62bbf37a01bc7f3dacc90c2ee90a4a7
+ <+>
2022-10-25 01:17:33 -0400

    fixes routes in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
 import NavBar from './NavBar';
-import Login from './Login';
-import JournalForm from './JournalForm';
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import JournalForm from "./JournalForm";
 import JournalList from './JournalList';
 import JournalPreview from './JournalPreview'
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [journals, setJournals] = useState([]);
+  console.log(setJournals)
 
-  const [journallist, setJournallist] = useState([])
-
-  useEffect (() => {
+  useEffect(() => {
     fetch("http://localhost:9292/journal_entries")
-      .then(r => r.json())
-      .then(data =>setJournallist)
-  }, [])
- 
- 
+      .then((r) => r.json())
+      .then((data) => setJournals(data));
+  }, []);
 
+  function handleDeleteJournal(deletedJournal) {
+    const updatedJournals = journals.filter((journal) => journal.id !== deletedJournal.id)
+    setJournals(updatedJournals)
+  }
   return (
     <div className="App">
-      hello
-
-    <Router>
+      APP
+      
+      {/* <Login setUser={setUser} /> */}
+      {/* {user ? (
+        <JournalForm setJournals={setJournals} user={user} />
+        ) : null}
+      */}
       <NavBar />
-      <LogIn />
-      <Switch>
-      
-        {/* <Login /> */}
-            
-        <Route exact path="/journal_entries/new" render={() => <JournalForm setJournallist={setJournallist} /> } />
-       
-        {/* <Route exact path="/">
-          <LogIn /> or <Home />
-        </Route>      */}
-      
-        <Route exact path="/journal_entries" render={() => <JournalList journallist={journallist} />} />
-           
-        <Route exact path="/journal_entries/:id" render={() => <JournalPreview /> } />
-        
-      </Switch>
-    </Router>
+      <Routes>
+        <Route exact path="/journal_entries/new" element={<JournalForm setJournals={setJournals} />}
+        />
+
+        <Route exact path="/" element={<Login setUser={setUser} />} />
+          {/* <LogIn /> or <Home /> */} 
+
+        <Route exact path="/journal_entries" element={<JournalList journals={journals} handleDeleteJournal/>} />
+
+        <Route exact path="/journal_entries/:id" element={<JournalPreview />} />
+      </Routes>
     </div>
   );
 }
