439f4288c056555bda157110dd892446b516f0d4
+ <+>
2022-10-25 01:20:57 -0400

    adds handle delete

--- src/components/JournalList.js
+++ src/components/JournalList.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import JournalCard from './JournalCard'
+import React from "react";
 
-function JournalList({ journallist }) {
 
-  const journalentries = journallist.map((entry) => 
-    <JournalCard id={entry.id} title={entry.title} content={entry.content} date={entry.date} /> )
-  
-  
-  
-  
-  return (
-    <div className="journal-list">{journalentries}</div>
-  )
+function JournalList({ journals, handleDeleteJournal }) {
+  const journalItems = journals.map((journalObj, index) => {
+    
+  function handleDeleteClick() {
+      fetch(`http://localhost:9292/journal_entries/${journalObj.id}`, {
+        method: "DELETE",
+      })
+        .then((r) =>r.json())
+        .then((deletedJournal) => handleDeleteJournal(deletedJournal))
+      }
+
+    return (
+      <div key={index}>
+        <p>{journalObj.title}</p>
+        <p>{journalObj.date}</p>
+        <p>{journalObj.content}</p>
+        <button onClick={handleDeleteClick}> Delete </button>
+      </div>
+    );
+  });
+
+  return <div className="journal-list">{journalItems}</div>;
 }
 
-export default JournalList
+export default JournalList;
